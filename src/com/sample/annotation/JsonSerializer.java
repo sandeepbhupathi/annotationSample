c46292cc2f0050ab46df51ae84a5ad56d2aadb9a
sandeepbupathi08 <sandeepbupathi08@gmail.com>
2018-02-11 09:59:29 +0530

    added few changes to the serializer.

--- src/com/sample/annotation/JsonSerializer.java
+++ src/com/sample/annotation/JsonSerializer.java
@@ -1,8 +1,12 @@
 package com.sample.annotation;
 
+import sun.reflect.generics.tree.Tree;
+
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.joining;
 
@@ -10,24 +14,26 @@ public class JsonSerializer {
     public String serialize(Object object) throws JsonSerializeException {
         try {
             Class<?> objectClass = requireNonNull(object).getClass();
-            Map<String, Object> jsonElements = new HashMap<>();
+            TreeMap<String, Object> jsonElements = new TreeMap<>();
             for (Field field: objectClass.getDeclaredFields()) {
                 field.setAccessible(true);
                 if (field.isAnnotationPresent(JsonField.class)) {
                     jsonElements.put(getSerializedKey(field), field.get(object));
                 }
             }
-            System.out.println(toJsonString(jsonElements));
             return toJsonString(jsonElements);
         }
         catch (IllegalAccessException e) {
             throw new JsonSerializeException(e.getMessage());
         }
     }
-    private String toJsonString(Map<String, Object> jsonMap) {
-        String elementsString = jsonMap.entrySet()
+    private String toJsonString(TreeMap<String, Object> jsonMap) {
+        String elementsString = jsonMap.descendingMap().entrySet()
                 .stream()
-                .map(entry -> "\""  + entry.getKey() + "\":\"" + entry.getValue() + "\"")
+                .map(entry -> { if(entry.getValue() instanceof String)
+                                    return "\""  + entry.getKey() + "\":\"" + entry.getValue() + "\"";
+                        return "\""  + entry.getKey() + "\":" + entry.getValue()  ;
+                            })
                 .collect(joining(","));
         return "{" + elementsString + "}";
     }
